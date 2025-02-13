6113c6eaef7db638f508f3b53530bd6fff97a223
Lars Kotthoff <lars@larsko.org>
2025-02-13 13:27:56 -0700

    fix lint

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
-// eslint-disable-next-line no-unused-vars
 function factorial(n) {
     if(n === 0) return 1;
     else return n * factorial(n - 1);
 }
 
+// eslint-disable-next-line no-unused-vars
 function e(n) {
     if(n === 0) return 1;
     else return 1.0 / factorial(n) + e(n - 1);
